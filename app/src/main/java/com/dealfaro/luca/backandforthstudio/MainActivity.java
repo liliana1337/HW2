fe403da2b214a21f28714013e0b6cac6afd5e6fa
Liliana <liliana1337@gmail.com>
2017-04-23 21:01:53 -0700

    h2 complete

--- app/src/main/java/com/dealfaro/luca/backandforthstudio/MainActivity.java
+++ app/src/main/java/com/dealfaro/luca/backandforthstudio/MainActivity.java
@@ -97,6 +97,7 @@ public class MainActivity extends AppCompatActivity {
       //Intent intent = new Intent(this, MainActivity.class);
       //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
       //startActivity(intent);
+        
     }
 
 
